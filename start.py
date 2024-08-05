fd9662d283b82f08bfe3a2ce68f5faec88bb1cfd
dw33d <170299648+dw33d@users.noreply.github.com>
2024-08-06 00:15:29 +0200

    First app idea commit

---
+++ start.py
@@ -0,0 +1,25 @@
+# Jimmy is creating a basic operating system for a school project
+# He is being marked on his user validation system. But has no clue on what to do
+# Help him make a basic validator that takes 3 chances 
+# to input a password correctly and shows the amount of chances left
+
+# Creating a counter for input chances and setting the user password
+counter = 3
+password = "Jimmy"
+
+# Loop for validating user input-password by comparing to set-password
+while True:
+    input_psswd = input("Welcome to BubbleOS! Enter your password to continue: ")
+    if input_psswd == password:
+        print("Welcome Jimmy!")
+        # stop if the same
+        break
+    elif input_psswd != password:
+        print("Incorrect Password")
+        # Decrease the amount of choices and let the user know how many chances they have left
+        counter = counter - 1
+        print(f"Number of chances left: {counter}")
+        if counter == 0:
+            print("Incorrect password...try again later")
+            # Stop everything if correct a third time
+            break
